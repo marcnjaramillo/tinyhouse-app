f30345152d9a4688f32383b88ce95458de2ad634
marcnjaramillo <marcn.jaramillo@gmail.com>
2020-01-29 22:15:59 -0800

    Set up basic GraphQL schema and Apollo server

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,27 +1,13 @@
 import express from 'express';
+import { ApolloServer } from 'apollo-server-express';
 
-import { listings } from './listings';
+import { schema } from './graphql';
 
 const app = express();
+const server = new ApolloServer({ schema });
 const port = 9000;
 
-app.use(express.json());
-
-app.get('/listings', (_req, res) => {
-  res.send(listings);
-});
-
-app.post('/delete-listing', (req, res) => {
-  const id: string = req.body.id;
-
-  for (let i = 0; i < listings.length; i++) {
-    if (listings[i].id === id) {
-      return res.send(listings.splice(i, 1)[0]);
-    }
-  }
-
-  return res.send('failed to deleted listing');
-});
+server.applyMiddleware({ app, path: '/api' });
 
 app.listen(port);
 
