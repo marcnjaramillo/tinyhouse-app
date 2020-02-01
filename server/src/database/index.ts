656dd17b1e4a3c650d889ec854252142e1f0c5cd
marcnjaramillo <marcn.jaramillo@gmail.com>
2020-01-31 22:19:41 -0800

    Use env variables

--- server/src/database/index.ts
+++ server/src/database/index.ts
@@ -2,11 +2,7 @@ import { MongoClient } from 'mongodb';
 
 import { Database } from '../lib/types';
 
-const user = 'XXXXX';
-const userPassword = 'XXXXXX';
-const cluster = 'cluster0-n35od';
-
-const url = `mongodb+srv://${user}:${userPassword}@${cluster}.mongodb.net`;
+const url = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_USER_PASSWORD}@${process.env.DB_CLUSTER}.mongodb.net`;
 
 export const connectDatabase = async (): Promise<Database> => {
   const client = await MongoClient.connect(url, {
