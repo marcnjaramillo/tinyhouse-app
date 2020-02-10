537fa0cce6d8c14cadec4c3c7cb770c11184ab1b
marcnjaramillo <marcn.jaramillo@gmail.com>
2020-02-09 22:49:28 -0800

    Add Login component and update layout

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,18 +1,30 @@
 import React from 'react';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
-import { Home, Host, Listing, Listings, NotFound, User } from './sections';
+import { Layout } from 'antd';
+import {
+  Home,
+  Host,
+  Listing,
+  Listings,
+  Login,
+  NotFound,
+  User
+} from './sections';
 
 const App = () => {
   return (
     <Router>
-      <Switch>
-        <Route exact path='/' component={Home} />
-        <Route exact path='/host' component={Host} />
-        <Route exact path='/listing/:id' component={Listing} />
-        <Route exact path='/listings/:location?' component={Listings} />
-        <Route exact path='/user/:id' component={User} />
-        <Route component={NotFound} />
-      </Switch>
+      <Layout id='app'>
+        <Switch>
+          <Route exact path='/' component={Home} />
+          <Route exact path='/host' component={Host} />
+          <Route exact path='/listing/:id' component={Listing} />
+          <Route exact path='/listings/:location?' component={Listings} />
+          <Route exact path='/user/:id' component={User} />
+          <Route exact path='/login' component={Login} />
+          <Route component={NotFound} />
+        </Switch>
+      </Layout>
     </Router>
   );
 };
