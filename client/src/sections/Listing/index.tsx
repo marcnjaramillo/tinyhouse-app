3cef8efad5ed6b4d62c300ebc8704e5e914f8f68
marcnjaramillo <marcn.jaramillo@gmail.com>
2020-02-17 15:06:37 -0800

    Add new components to Listing component

--- client/src/sections/Listing/index.tsx
+++ client/src/sections/Listing/index.tsx
@@ -1,13 +1,19 @@
 import React, { useState } from 'react';
 import { RouteComponentProps } from 'react-router-dom';
 import { useQuery } from '@apollo/react-hooks';
-import { Layout } from 'antd';
+import { Col, Layout, Row } from 'antd';
+import { Moment } from 'moment';
 import { ErrorBanner, PageSkeleton } from '../../lib/components';
 import { LISTING } from '../../lib/graphql/queries';
 import {
   Listing as ListingData,
   ListingVariables
 } from '../../lib/graphql/queries/Listing/__generated__/Listing';
+import {
+  ListingCreateBooking,
+  ListingBookings,
+  ListingDetails
+} from './components';
 
 interface MatchParams {
   id: string;
@@ -17,8 +23,9 @@ const { Content } = Layout;
 const PAGE_LIMIT = 3;
 
 export const Listing = ({ match }: RouteComponentProps<MatchParams>) => {
-  // eslint-disable-next-line @typescript-eslint/no-unused-vars
   const [bookingsPage, setBookingsPage] = useState(1);
+  const [checkInDate, setCheckInDate] = useState<Moment | null>(null);
+  const [checkOutDate, setCheckOutDate] = useState<Moment | null>(null);
 
   const { loading, data, error } = useQuery<ListingData, ListingVariables>(
     LISTING,
@@ -49,7 +56,42 @@ export const Listing = ({ match }: RouteComponentProps<MatchParams>) => {
   }
 
   const listing = data ? data.listing : null;
-
-  // eslint-disable-next-line @typescript-eslint/no-unused-vars
   const listingBookings = listing ? listing.bookings : null;
+
+  const listingDetailsElement = listing ? (
+    <ListingDetails listing={listing} />
+  ) : null;
+
+  const listingBookingsElement = listingBookings ? (
+    <ListingBookings
+      listingBookings={listingBookings}
+      bookingsPage={bookingsPage}
+      limit={PAGE_LIMIT}
+      setBookingsPage={setBookingsPage}
+    />
+  ) : null;
+
+  const listingCreateBookingElement = listing ? (
+    <ListingCreateBooking
+      price={listing.price}
+      checkInDate={checkInDate}
+      checkOutDate={checkOutDate}
+      setCheckInDate={setCheckInDate}
+      setCheckOutDate={setCheckOutDate}
+    />
+  ) : null;
+
+  return (
+    <Content className='listings'>
+      <Row gutter={24} type='flex' justify='space-between'>
+        <Col xs={24} lg={14}>
+          {listingDetailsElement}
+          {listingBookingsElement}
+        </Col>
+        <Col xs={24} lg={10}>
+          {listingCreateBookingElement}
+        </Col>
+      </Row>
+    </Content>
+  );
 };
