13c334003dcab13d2112a76fddcd02b6fbe72b32
marcnjaramillo <marcn.jaramillo@gmail.com>
2020-02-23 23:15:47 -0800

    Add search functionality and allow access to history

--- client/src/sections/AppHeader/index.tsx
+++ client/src/sections/AppHeader/index.tsx
@@ -1,10 +1,11 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
-import { Layout } from 'antd';
+import React, { useState, useEffect } from 'react';
+import { Link, withRouter, RouteComponentProps } from 'react-router-dom';
+import { Input, Layout } from 'antd';
 import { Viewer } from '../../lib/types';
 import { MenuItems } from './components';
 
 import logo from './assets/tinyhouse-logo.png';
+import { displayErrorMessage } from '../../lib/utils';
 
 interface Props {
   viewer: Viewer;
@@ -12,20 +13,55 @@ interface Props {
 }
 
 const { Header } = Layout;
+const { Search } = Input;
 
-export const AppHeader = ({ viewer, setViewer }: Props) => {
-  return (
-    <Header className='app-header'>
-      <div className='app-header__logo-search-section'>
-        <div className='app-header__logo'>
-          <Link to='/'>
-            <img src={logo} alt='App logo' />
-          </Link>
+export const AppHeader = withRouter(
+  ({ viewer, setViewer, location, history }: Props & RouteComponentProps) => {
+    const [search, setSearch] = useState('');
+    useEffect(() => {
+      const { pathname } = location;
+      const pathnameSubStrings = pathname.split('/');
+      if (!pathname.includes('/listings')) {
+        setSearch('');
+        return;
+      }
+
+      if (pathname.includes('/listings') && pathnameSubStrings.length === 3) {
+        setSearch(pathnameSubStrings[2]);
+        return;
+      }
+    }, [location]);
+    const onSearch = (value: string) => {
+      const trimmedValue = value.trim();
+
+      if (trimmedValue) {
+        history.push(`/listings/${trimmedValue}`);
+      } else {
+        displayErrorMessage('Please enter a valid search');
+      }
+    };
+    return (
+      <Header className='app-header'>
+        <div className='app-header__logo-search-section'>
+          <div className='app-header__logo'>
+            <Link to='/'>
+              <img src={logo} alt='App logo' />
+            </Link>
+          </div>
+          <div className='app-header__search-input'>
+            <Search
+              placeholder="Search 'San Fransisco'"
+              enterButton
+              value={search}
+              onChange={evt => setSearch(evt.target.value)}
+              onSearch={onSearch}
+            />
+          </div>
         </div>
-      </div>
-      <div className='app-header__menu-section'>
-        <MenuItems viewer={viewer} setViewer={setViewer} />
-      </div>
-    </Header>
-  );
-};
+        <div className='app-header__menu-section'>
+          <MenuItems viewer={viewer} setViewer={setViewer} />
+        </div>
+      </Header>
+    );
+  }
+);
