a2ebf54c66df93e6f677d0797d2d4b59f3809ebb
marcnjaramillo <marcn.jaramillo@gmail.com>
2020-03-03 23:39:25 -0800

    Add bodyParser to server

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,15 +1,16 @@
 require('dotenv').config();
 
 import express, { Application } from 'express';
+import bodyParser from 'body-parser';
 import cookieParser from 'cookie-parser';
 import { ApolloServer } from 'apollo-server-express';
-
 import { connectDatabase } from './database';
 import { typeDefs, resolvers } from './graphql';
 
 const mount = async (app: Application) => {
   const db = await connectDatabase();
 
+  app.use(bodyParser.json({ limit: '2mb' }));
   app.use(cookieParser(process.env.SECRET));
 
   const server = new ApolloServer({
@@ -21,7 +22,7 @@ const mount = async (app: Application) => {
   server.applyMiddleware({ app, path: '/api' });
   app.listen(process.env.PORT);
 
-  console.log(`[app] : http://localhost${process.env.PORT}`);
+  console.log(`[app] : http://localhost:${process.env.PORT}`);
 };
 
 mount(express());
