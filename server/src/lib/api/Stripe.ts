4b04dc889b67321bf3783f083e250169778bca43
marcnjaramillo <marcn.jaramillo@gmail.com>
2020-03-09 23:51:41 -0700

    Add ability for making charges

--- server/src/lib/api/Stripe.ts
+++ server/src/lib/api/Stripe.ts
@@ -10,8 +10,28 @@ export const Stripe = {
       /* eslint-disable @typescript-eslint/camelcase */
       grant_type: 'authorization_code',
       code
+      /* eslint-enable @typescript-eslint/camelcase */
     });
 
     return response;
+  },
+  charge: async (amount: number, source: string, stripeAccount: string) => {
+    /* eslint-disable @typescript-eslint/camelcase */
+    const res = await client.charges.create(
+      {
+        amount,
+        currency: 'usd',
+        source,
+        application_fee_amount: Math.round(amount * 0.05)
+      },
+      {
+        stripe_account: stripeAccount
+      }
+    );
+    /* eslint-enable @typescript-eslint/camelcase */
+
+    if (res.status !== 'succeeded') {
+      throw new Error('failed to create charge with Stripe');
+    }
   }
 };
