8ab3b6cfa81745000ab04ce473ec62bad8b223cc
marcnjaramillo <marcn.jaramillo@gmail.com>
2020-04-28 00:06:49 -0700

    Prepare app for deployment

---
+++ tinyhouse-deploy/src/client/precache-manifest.662cbae5dd77a951f2d584284736662e.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "de49d37b03c8344315f0e50d58bdf82f",
+    "url": "/index.html"
+  },
+  {
+    "revision": "579302d7c2242acd570f",
+    "url": "/static/css/2.c11cfb7b.chunk.css"
+  },
+  {
+    "revision": "747d051b85d35facf782",
+    "url": "/static/css/main.7a968d50.chunk.css"
+  },
+  {
+    "revision": "579302d7c2242acd570f",
+    "url": "/static/js/2.4184fe11.chunk.js"
+  },
+  {
+    "revision": "8e87f68634b1e5b84f6f7c9aeab3aa68",
+    "url": "/static/js/2.4184fe11.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "747d051b85d35facf782",
+    "url": "/static/js/main.d636ddfd.chunk.js"
+  },
+  {
+    "revision": "d230685fb04f7ab82483",
+    "url": "/static/js/runtime-main.ca47c4cd.js"
+  },
+  {
+    "revision": "cd98e2c39b50532ef09a3344e820499f",
+    "url": "/static/media/cancun.cd98e2c3.jpg"
+  },
+  {
+    "revision": "df994899b2925871e24126812c416c24",
+    "url": "/static/media/dubai.df994899.jpg"
+  },
+  {
+    "revision": "841651c32f46a04c181eb090a32bd103",
+    "url": "/static/media/london.841651c3.jpg"
+  },
+  {
+    "revision": "73d8ec963252e42850a528642374f476",
+    "url": "/static/media/los-angeles.73d8ec96.jpg"
+  },
+  {
+    "revision": "c21df1475cabff80a179b3c0cd49dd67",
+    "url": "/static/media/map-background.c21df147.jpg"
+  },
+  {
+    "revision": "23aadf8146a0240ddc18f846d8c64843",
+    "url": "/static/media/san-fransisco.23aadf81.jpg"
+  },
+  {
+    "revision": "3c573d340e505c3f2f5b60c34b20aacc",
+    "url": "/static/media/toronto.3c573d34.jpg"
+  }
+]);
